7ef9536dc2e00122695560714548514da832b98d
armancj <76697120+armancj@users.noreply.github.com>
2022-12-06 04:55:42 -0500

    adding tag to profile controller

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -12,7 +12,9 @@ import { PostsService } from './posts.service';
 import { CreatePostDto } from './dto/create-post.dto';
 import { UpdatePostDto } from './dto/update-post.dto';
 import { GetAllQueryDto } from '../common/dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Posts')
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) {}
