be04b202a699c52d693341a23d95e1775f754c16
mandi <armancanjer@gmail.com>
2023-04-07 12:35:31 -0400

    update template correo

--- src/minio-storage/dto/update-minio-storage.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateMinioStorageDto } from './create-minio-storage.dto';
-
-export class UpdateMinioStorageDto extends PartialType(CreateMinioStorageDto) {}
