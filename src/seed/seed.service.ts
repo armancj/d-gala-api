d45310043b0f26af9f73c6c851c0fb73e4c88cd0
armancj <armancanjer@gmail.com>
2023-04-24 09:00:25 -0400

    adding config service to seed service

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -2,10 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CategoryData } from '../products/category/data/category.data';
 import { HandlerError } from '../common/utils/handler-error';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class SeedService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly configServices: ConfigService,
+  ) {}
 
   async executeSeed() {
     await this.prisma.category.deleteMany({});
